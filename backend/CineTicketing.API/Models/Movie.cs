38e2b16313b6b3ca59c1f8930447baedaee01173
Rodrigo Sídney Colque Quispe <rc2023077078@virtual.upt.pe>
2026-04-25 08:34:06 -0500

    Add Movie model with properties

---
+++ backend/CineTicketing.API/Models/Movie.cs
@@ -0,0 +1,8 @@
+namespace CineTicketing.API.Models;
+
+public class Movie {
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Genre { get; set; } = string.Empty;
+    public int DurationMinutes { get; set; }
+}
